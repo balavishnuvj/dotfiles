6e10ef738d66945b8106ade9007f6a14481866d8
Apple <apple@Apples-MacBook-Pro.local>
2021-08-25 12:07:04 +0530

    typo in url

--- setup.sh
+++ setup.sh
@@ -1,15 +1,16 @@
 #!/bin/zsh 
 
 git config --global push.default current
+git config --global pull.ff only
 
-# cd ~/Workspaces
+# cd ~/Workspace
 # git clone https://github.com/balavishnuvj/dotfiles.git
-DOTFILE_LOCATION="${HOME}/Workspaces/dotfiles"
+# DOTFILE_LOCATION="${HOME}/Workspace/dotfiles"
 
 # ZSH_PLUGIN_PATH=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins
 # echo "Cloning ZSH Plugins"
 # git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_PLUGIN_PATH}/zsh-autosuggestions
 
-echo "Adding common zshrc in base zshrc"
+# echo "Adding common zshrc in base zshrc"
 # echo "source ${DOTFILE_LOCATION}/common-zshrc" >> ${HOME}/.zshrc
-source "${HOME}/.zshrc"
+# source "${HOME}/.zshrc"
